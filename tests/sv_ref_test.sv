a948bc5f9086264e70afbcc038edc7092bc4f6d2
sschip <sschip@163.com>
2024-01-26 00:17:59 -0700

    Add files via upload

---
+++ tests/sv_ref_test.sv
@@ -0,0 +1,20 @@
+// module definition
+module_000 u_module(
+);
+
+module_001 u_module();
+
+module_002 
+u_module();
+
+module_003 
+u_module
+();
+
+module_004 #(.PARAM_000(100))
+u_module
+();
+// class definition
+class_000 class_inst;
+// task definition
+// function definition
--- tests/sv_test.sv
+++ tests/sv_test.sv
@@ -1,1 +1,26 @@
+// module definition
+module module_000(
+);
 
+module module_001();
+
+module 
+module_002();
+
+module 
+module_003
+();
+
+module #(parameter PARAM_000 = 100)
+module_004
+();
+// class definition
+class class_000;
+endclass
+// task definition
+// function definition
+function uvm_component uvm_compnent::function_000 (string requested_type_name,
+                                                       string name);
+                                                   return factory.create_component_by_name(requested_type_name, get_full_name(),
+                                                                                           name, this);
+endfunction
